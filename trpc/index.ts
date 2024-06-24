3a17ceafa215742b8d66a6f0c7ad29a9f92108bc
LarinNikita <larin_nikita_m@mail.ru>
2024-06-24 11:35:16 +0700

    deleting file function

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,6 +1,6 @@
 import { TRPCError } from '@trpc/server'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-
+import { z } from 'zod'
 import { privateProcedure, publicProcedure, router } from './trpc'
 import { db } from '@/db'
 
@@ -36,6 +36,28 @@ export const appRouter = router({
             },
         })
     }),
+    deleteFile: privateProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const { userId } = ctx
+            const file = await db.file.findFirst({
+                where: {
+                    id: input.id,
+                    userId,
+                },
+            })
+
+            if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+            await db.file.delete({
+                where: {
+                    id: input.id,
+                    userId,
+                },
+            })
+
+            return file
+        }),
 })
 
 export type AppRouter = typeof appRouter
