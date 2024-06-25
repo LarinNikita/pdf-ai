4fedcc0d969fda75ad0902caa766f7b9c61190f6
LarinNikita <larin_nikita_m@mail.ru>
2024-06-25 11:25:46 +0700

    chat input ui

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { Textarea } from '../ui/textarea'
+import { Button } from '../ui/button'
+import { Send } from 'lucide-react'
 
 interface ChatInputProps {
     isDisabled?: boolean
 }
 
 const ChatInput = ({ isDisabled }: ChatInputProps) => {
-    return <div>ChatInput</div>
+    return (
+        <div className="absolute bottom-0 left-0 w-full">
+            <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+                <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+                    <div className="relative flex w-full flex-grow flex-col p-4">
+                        <div className="relative">
+                            <Textarea
+                                rows={1}
+                                maxRows={4}
+                                autoFocus
+                                placeholder="Enter your question..."
+                                className="scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch resize-none py-3 pr-12 text-base"
+                            />
+                            <Button
+                                aria-label="send message"
+                                className="absolute bottom-1.5 right-[8px]"
+                            >
+                                <Send className="size-4" />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default ChatInput
